3ec8496e6255d17fa1ef847452d62ac0945dcb01
Farhan Khan <mfarhan133@gmail.com>
2019-08-03 21:09:14 +0500

    Practice TTD on django framework

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+AUTH_USER_MODEL = 'core.User'
 STATIC_URL = '/static/'
