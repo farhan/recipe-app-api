dac449ebcea9db62d0200260200cad74c806a1f6
Farhan Khan <mfarhan133@gmail.com>
2019-08-10 13:17:27 +0500

    Add working list tag endpoints

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
